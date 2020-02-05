57fa56bf9327ca6830f054bc97b39c5a6aa9e7a0
Martin Bálint <bamrtn@gmail.com>
2020-02-05 14:13:33 +0000

    Update kml reader

--- reader.py
+++ reader.py
@@ -4,9 +4,9 @@ import numpy as np
 
 def parseKML(inputfile):
   with open(inputfile, "r") as f:
-    soup = BeautifulSoup(f, features="html5lib")
-    return np.array([[i.split(",")[0:2] for i in node.string.split()]\
-            for node in soup.findAll("coordinates")]).astype(np.float)
+    soup = BeautifulSoup(f, features="html.parser")
+    return [np.array([i.split(",")[0:2] for i in node.string.split()]).astype(np.float)\
+            for node in soup.findAll("coordinates")]
 
 if __name__ == "__main__":
   for array in parseKML("2017_polygons.kml"):
