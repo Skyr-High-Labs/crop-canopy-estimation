ad8bb427279663389cad3a02b6885af4078c7060
David Khachaturov <d77777k@gmail.com>
2020-02-24 09:39:06 +0000

    Updated requirement.txt; Implemented saving/load model in train_and_test (untested)

--- model.py
+++ model.py
@@ -4,6 +4,10 @@ from sklearn.model_selection import train_test_split
 import random
 import matplotlib.pyplot as plt
 
+from datetime import datetime
+from joblib import dump, load
+import os.path
+
 """
 Creates a dataset of SAR-NDVI values in the following format:
 
@@ -17,38 +21,43 @@ Labels:
  * NDVI
 
 The return value is the (X, y), where
-	X = [[vv_0,vh_0,nrpb_0,lia_0],...,[vv_n,vh_n,nrpb_n,lia_n]]
-	y = [ndvi_0,...,ndvi_n]
+    X = [[vv_0,vh_0,nrpb_0,lia_0],...,[vv_n,vh_n,nrpb_n,lia_n]]
+    y = [ndvi_0,...,ndvi_n]
 """
 def make_dataset():
-	# TODO: replace with data from Earth Engine
-	return [[-0.5 + i/100 + random.random(), 0.0, 0.0] for i in range(100)], \
-			[i/100 for i in range(100)]
+    # TODO: replace with data from Earth Engine
+    return [[-0.5 + i/100 + random.random(), 0.0, 0.0] for i in range(100)], \
+            [i/100 for i in range(100)]
 
 
-def train_and_test(X, y, test_split=0.33):
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_split, random_state=42)
-	regr = RandomForestRegressor()
-	regr.fit(X_train, y_train)
-
-	print("Train set size: {}".format(len(y_train)))
-	print("Test set size: {}".format(len(y_test)))
-
-	score = regr.score(X_test, y_test)
-	print("Regression score: R^2={}".format(score))
-
-	print(regr.get_params(deep=True))
-	print(regr.feature_importances_)
-	#print()
-	#print("Example prediction: true={} predicted={}".format(regr.predict(X_test[:1])[0], y_test[0]))
-	
-	#plt.scatter(regr.predict(X_test), y_test)
-	#plt.plot([-1.,1.],[-1.,1.], ls="--", c="r")
-	#plt.xlim(-1,1)
-	#plt.ylim(-1,1)
-	#plt.show()
+def train_and_test(X, y, test_split=0.33, file_name=("model_" + str(datetime.today().timestamp())):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_split, random_state=42)
+    regr = None
+    if os.path.isfile(file_name):
+        print(f"Loading existing file {file_name}...")
+        regr = load(file_name)
+    else:
+        print("Training new model...")
+        print("Train set size: {}".format(len(y_train)))
+        print("Test set size: {}".format(len(y_test)))
+             
+        regr = RandomForestRegressor()
+        regr.fit(X_train, y_train)
+        
+        print(f"Saving to {file_name}...")
+        dump(regr, file_name)
+        print("Done!")
+    print("Scoring...")
+    score = regr.score(X_test, y_test)
+    print("Regression score: R^2={}".format(score))
+    
+    #plt.scatter(regr.predict(X_test), y_test)
+    #plt.plot([-1.,1.],[-1.,1.], ls="--", c="r")
+    #plt.xlim(-1,1)
+    #plt.ylim(-1,1)
+    #plt.show()
 
 
 if __name__ == '__main__':
-	X, y = make_dataset()
-	train_and_test(X, y)
+    X, y = make_dataset()
+    train_and_test(X, y)
