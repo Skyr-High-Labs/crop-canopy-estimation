8c285476b83a52a21402cc105065c6b315ffb67d
Shiv <shivgunter@gmail.com>
2020-02-25 18:28:43 +0000

    should be done

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
 from flask_restplus import Api, Resource, fields
-import os
 from joblib import dump, load
-from pixelPairs import pixelPairs
+import os
+import getSAR
 
 flask_app = Flask(__name__)
 app = Api(app = flask_app,
@@ -13,13 +13,19 @@ app = Api(app = flask_app,
 name_space = app.namespace('cce', description='Estimate Crop Canopy Cover')
 
 model = app.model('CCE Model',
-                  {'name': fields.String(required=True,
-                                         description="Name of the person",
-                                         help="Name cannot be blank.")})
+                  {'polygon': fields.List(required=True,
+                                         description="Polygon for a field",
+                                         help="Array of coordinates (2-tuples)"),
+                    'startDate': fields.DateTime(required=True,
+                                            description="Start date",
+                                            help="Start date"),
+                  'endDate': fields.DateTime(required=True,
+                                             description="End date",
+                                             help="End date")})
                                          
 # TODO: sort out parameters
-@app.doc(responses={200: 'OK', 400: 'Error'},
-         params={'placeholder': 'description'})
+@app.doc(responses={200: 'OK', 400: 'Error', 500: 'Mapping Key Error'},
+         params={})
 
 @name_space.route("/")
 class MainClass(Resource):
@@ -29,27 +35,33 @@ class MainClass(Resource):
             "status": "Did nothing, use POST Request"
         }
 
+    @app.expect(model)
     def post(self):
         try:
-            # TODO: get the data FROM the post request
-            polygon = None
-            startDate = None
-            endDate = None
+            # get the data from the post request
+
+            polygon = request.json["polygon"]
+            startDate = request.json["startDate"]
+            endDate = request.json["endDate"]
             # load model
             # TODO: how is file_name decided?
-            file_name = None
+            file_name = None # fill this in later
             if os.path.isfile(file_name):
                 print(f"Loading existing file {file_name}...")
                 regr = load(file_name)
-                # TODO: clearly y can't be generated. So how is this function modified?
-                X, _ = pixelPairs(polygon, startDate, endDate)
+                X = getSAR.arrayToSAR(polygon, startDate, endDate)
                 y = regr.predict(X)
                 return {
                     "status": "Done",
                     "ndvi": y
                 }
             else:
-                raise Exception("Model file not available")
+                raise FileNotFoundError
+        except FileNotFoundError:
+            name_space.abort(400, e.__doc__, status="Model file could not be found", statusCode="400")
+        except KeyError as e:
+            name_space.abort(
+                500, e.__doc__, status="Could not find key", statusCode="500")
         except Exception as e:
-            name_space.abort(400, e.__doc__, status = e.value, statusCode = "400")
+            name_space.abort(400, e.__doc__, status ="Unspecified Error", statusCode = "400")
         
